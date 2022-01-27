0d46fb7aecc6069a32d1c51fe534f0403153d743
Rémi Jarjat <remi@drakona.fr>
2022-01-27 22:08:02 +0100

    Exercice 8 à 13

--- includes/variables.php
+++ includes/variables.php
@@ -1,25 +1,31 @@
 <?php
 $description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida.";
 
+
+// Images téléchargées sur https://www.headict.com/fr/101-bonnet
 $beanies = [
-    [
+    42 => [
         "name"        => "Bonnet en laine",
         "description" => $description,
         "price"       => 10.0,
+        "image"       => 'the-fjord-black.jpg',
     ],
-    [
+    12 => [
         "name"        => "Bonnet en laine bio",
         "description" => $description,
         "price"       => 14.0,
+        "image"       => 'the-uniform-low-red-clay.jpg',
     ],
-    [
+    2 => [
         "name"        => "Bonnet en laine et cachemire",
         "description" => $description,
         "price"       => 20.0,
+        "image"       => 'merino-beanie-navy.jpg',
     ],
-    [
+    666 => [
         "name"        => "Bonnet arc-en-ciel",
         "description" => $description,
         "price"       => 12.0,
+        "image"       => 'the-yukon-brim-olive.jpg',
     ],
 ];
