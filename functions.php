85b3a7e885b4bdc3f0418c0b485e992f36d52c25
Rémi Jarjat <remi@drakona.fr>
2022-01-27 21:25:06 +0100

    Exercices 1 à 7

---
+++ functions.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @param float $price
+ * @return float
+ */
+function withoutVAT(float $price): float
+{
+    // équivalent à $price / (1 + (20 / 100))
+    return $price / 1.2;
+}
+
+/**
+ * Convertit l'affichage d'un prix, au format français.
+ * Ex: 12 => 12,00
+ *
+ * @param float $price
+ * @return string
+ */
+function formatPrice(float $price): string
+{
+    return number_format($price, 2, ',', ' ');
+}
+
+/**
+ * Affiche une ligne (table HTML) d'un tableau de bonnets
+ *
+ * @param int $id Identifiant (clé) du bonnet
+ * @param array $beanie Les informations d'un bonnet
+ */
+function displayBeanieLine(int $id, array $beanie): void
+{
+    $color = 'blue';
+    if ($beanie['price'] <= 12.0) {
+        $color = 'green';
+    }
+?>
+    <tr>
+        <td>
+            <?= $id; ?>
+        </td>
+        <td>
+            <?= $beanie['name']; ?>
+        </td>
+        <td style="color: <?= $color; ?>;">
+            <?= formatPrice(withoutVAT($beanie['price'])); ?>€
+        </td>
+        <td style="color: <?= $color; ?>;">
+            <?= formatPrice($beanie['price']); ?>€
+        </td>
+        <td>
+            <?= $beanie['description']; ?>
+        </td>
+    </tr>
+<?php
+}
+?>
