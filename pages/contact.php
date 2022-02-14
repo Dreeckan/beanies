66230a90faa06cf5a6ce82269f555b1d5770fbc4
Rémi Jarjat <dreeckan@gmail.com>
2022-02-14 16:23:29 +0100

    25 - validation et traitement du formulaire de contact dans une classe

--- pages/contact.php
+++ pages/contact.php
@@ -1,40 +1,7 @@
 <?php
-$errors = [];
-$subject = '';
-$email = '';
-$content = '';
+$contact = new Contact($_POST);
 
-if (isset($_POST['subject'])) {
-    $subject = trim($_POST['subject']);
-
-    if (empty($subject)) {
-        $errors[] = "Veuillez entrer un sujet";
-    } elseif (strlen($subject) <= 10) {
-        $errors[] = "Veuillez entrer un sujet plus long (10 caractères minimum)";
-    }
-}
-
-if (isset($_POST['email'])) {
-    $email = trim($_POST['email']);
-
-    if (empty($email)) {
-        $errors[] = "Veuillez entrer un e-mail";
-    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = "Veuillez entrer un e-mail valide";
-    }
-}
-
-if (isset($_POST['content'])) {
-    $content = trim($_POST['content']);
-
-    if (empty($content)) {
-        $errors[] = "Veuillez entrer un message";
-    } elseif (strlen($content) <= 100) {
-        $errors[] = "Veuillez entrer un message plus long (100 caractères minimum)";
-    }
-}
-
-if (isset($_POST['content']) && empty($errors)) {
+if ($contact->isSubmitted() && $contact->isValid()) {
 ?>
     <div class="alert alert-success" role="alert">
         Message envoyé
@@ -42,7 +9,7 @@ if (isset($_POST['content']) && empty($errors)) {
 <?php
 }
 
-foreach ($errors as $error) {
+foreach ($contact->getErrors() as $error) {
 ?>
     <div class="alert alert-danger" role="alert">
         <?= $error; ?>
@@ -54,15 +21,15 @@ foreach ($errors as $error) {
 <form action="" method="post">
     <div class="mb-3">
         <label for="subject" class="form-label">Sujet</label>
-        <input type="text" class="form-control" id="subject" name="subject" value="<?= $subject; ?>">
+        <input type="text" class="form-control" id="subject" name="subject" value="<?= $contact->getSubject(); ?>">
     </div>
     <div class="mb-3">
         <label for="email" class="form-label">Adresse e-mail</label>
-        <input type="email" class="form-control" id="email" name="email" value="<?= $email; ?>">
+        <input type="email" class="form-control" id="email" name="email" value="<?= $contact->getEmail(); ?>">
     </div>
     <div class="mb-3">
         <label for="content" class="form-label">Votre message</label>
-        <textarea class="form-control" id="content" name="content"><?= $content; ?></textarea>
+        <textarea class="form-control" id="content" name="content"><?= $contact->getContent(); ?></textarea>
     </div>
     <button type="submit" class="btn btn-primary">Envoyer</button>
 </form>
